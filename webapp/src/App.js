276bff35a691f174f00a693d4357a839565b55f9
Kaylie Son <kayliemoa@gmail.com>
2018-02-05 23:37:18 -0800

    test1
    aa

---
+++ webapp/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Landing from './comp/Landing';
+import Chat from './comp/Chat';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state={
+            page:"",
+        }
+        this.changePage = this.changePage.bind(this);
+ 
+        
+    }
+    
+    changePage(data){
+        this.setState({
+            page:data
+        })
+    }
+
+    
+  render() {
+      var comp = null;
+      
+      if(this.state.page =="" ||this.state.page =="Landing"){
+          comp = <Landing changePage = {this.changePage} />
+      
+      }else if(this.state.page == "Chat"){
+          comp = <Chat changePage = {this.changePage} />
+      
+      }
+      
+    return (
+      <div className="App">
+        {comp}
+        
+      </div>
+    );
+  }
+}
+
+export default App;
