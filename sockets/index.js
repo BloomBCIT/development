39b05a8a07fefdd4ed65c3ff523aee9cf9fcaffb
Kaylie Son <kayliemoa@gmail.com>
2018-02-05 23:57:00 -0800

    test
    a

--- sockets/index.js
+++ sockets/index.js
@@ -1,3 +1,24 @@
+const express = require("express");
+const app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get("/", (req, resp) => {
+    resp.end("Hi Welcome to my Heroku.bye");
+})
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+        return false;
+    }
+    console.log("Port is runnig");
+})
+
+
+
+
+
 const server = require("http").Server();
 const port = 10001;
 
