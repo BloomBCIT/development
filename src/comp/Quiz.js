335d10ac20873dc1310ef7c0355fa9b4ba1f51f7
kaylie <mwg1021@hotmail.com>
2018-03-16 18:21:28 -0700

    1
    a

--- src/comp/Quiz.js
+++ src/comp/Quiz.js
@@ -7,7 +7,8 @@ class Quiz extends Component {
         this.state = {
             stage:0,
             host:null,
-            qobj:{q:null, o1:null, o2:null, a:null}
+            qobj:{q:null, o1:null, o2:null, a:null},
+            cupimg:require("../img/cup.png")
        
         }
        //this.handleStage = this.state.handleStage.bind(this); 
@@ -76,8 +77,12 @@ class Quiz extends Component {
         if(this.state.stage ===0){
               comp = (
                 <div className="whichRoom">
-                    <button onClick={this.handleStage.bind(this, 1, "room1")}>Room 1</button>
-                    <button onClick={this.handleStage.bind(this, 1, "room2")}>Room 2</button>
+                	<div className="insideWhichRoom">
+					<img src={this.state.cupimg} height={200}/><br/>
+					<p className="word">We would like to know your tea taste!</p><br/>
+                    <button className="roomBut3" onClick={this.handleStage.bind(this, 1, "room1")}>Room 1</button>
+                    <button className="roomBut4" onClick={this.handleStage.bind(this, 1, "room2")}>Room 2</button>
+               		</div>
                 </div>
 
             );  
@@ -86,9 +91,10 @@ class Quiz extends Component {
             //select to be either host / player
                 comp = (
                 <div>
-                    <button onClick={this.handlePlayers.bind(this, true)}>Host</button>
-                    <button onClick={this.handlePlayers.bind(this, false)}>Player</button>
-            
+                	<div className="insideWhichRoom">
+                 		<button className="roomBut3" onClick={this.handlePlayers.bind(this, true)}>Host</button>
+                  		<button className="roomBut4" onClick={this.handlePlayers.bind(this, false)}>Player</button>
+            		</div>
                 </div>
 
             );  
@@ -98,24 +104,31 @@ class Quiz extends Component {
             if(this.state.host ===true){
                 comp = (
                     <div>
-                        <input ref="q" type="text" placeholder="Type your question here" />
-                        <input ref="o1" type="text" placeholder="option1" />
-                        <input ref="o2" type="text" placeholder="option2" />
-                        <select ref="a">
-                            <option value="1">Option 1</option>
-                            <option value="2">Option 2</option>
-                        </select>
-                        <button onClick={this.handleQuestion}> Submit Question</button>
+                   		<div className="insideWhichRoom">
+                   		<p className="word">Make a question for tea discovery.</p><br/>
+                        <input className="typeQuestion" ref="q" type="text" placeholder="Type your question here" /><br/>
+                        <input className="option" ref="o1" type="text" placeholder="option1" />
+                        <input className="option" ref="o2" type="text" placeholder="option2" /><br/>
+                        <select className="typeQuestion" ref="a">
+                        	<option className="option" value="1">Choose Answer</option>
+                            <option className="option" value="1">Option 1</option>
+                            <option className="option" value="2">Option 2</option>
+                        </select><br/>
+                        <button className="roomBut5 question" onClick={this.handleQuestion}> Submit Question</button>
+                   		</div>
                     </div>
                 );
                 
             } else if(this.state.host ===false){
                 comp = (
                     <div className="showingRooms">
+                    
+                    	<div className="insideWhichRoom">
                         <div>{this.state.qobj.q}</div>
-                        <button onClick={this.handleAnswer.bind(this, "1")}>{this.state.qobj.o1}</button>
-                        <button onClick={this.handleAnswer.bind(this, "2")}>{this.state.qobj.o2}</button>
-                      
+                        
+                        <button className="option" onClick={this.handleAnswer.bind(this, "1")}>{this.state.qobj.o1}</button>
+                        <button className="option" onClick={this.handleAnswer.bind(this, "2")}>{this.state.qobj.o2}</button>
+                      	</div>
                     </div>
                 );
                 
